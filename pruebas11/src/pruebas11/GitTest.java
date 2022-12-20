82e65e64f98b708f5820bf577a75573ff3555072
a22marramgar <a22marramgar@inspedralbes.cat>
2022-12-20 12:11:52 +0100

    1d6

--- pruebas11/src/pruebas11/GitTest.java
+++ pruebas11/src/pruebas11/GitTest.java
@@ -2,7 +2,8 @@ package pruebas11;
 
 public class GitTest {
 	  public static void main(String[] args) {
-	    System.out.println("Git is fun");
+	    System.out.println(Math.random()*6+1);
+	    System.out.println("Hola");
 	  }
 	}
 
